1d98d8c4ab7da278c395c6ff5fe694eb12fa663d
Adwait <hirfse4t@gmail.com>
2026-03-21 22:46:45 +0530

    feat(api): implemented basic user handling and product handling in order controller

--- controllers/api/apiOrder.controller.js
+++ controllers/api/apiOrder.controller.js
@@ -1,2 +1,34 @@
 const Order = require('../../models/order.model')
+const Product = require('../../models/product.model')
 const User = require('../../models/user.model')
+
+
+exports.orderHandling = async(req,res) => {
+    try{
+        const {userId, porductId} = req.body
+        
+        const user = await User.findById(userId)
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found..!"
+            })
+        }
+
+        const product = await Product.findById(porductId)
+
+
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: "Product Not Found..!"
+            })
+        }
+
+        
+
+    }catch(error){
+
+    }
+}
