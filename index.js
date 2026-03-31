2869d007e718db17a29f3de5406a352b32ce23db
Adwait <hirfse4t@gmail.com>
2026-03-31 22:53:56 +0530

    chore(api): add debug log to verify JWT_SECRET in production

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const fs = require('fs');
 const nocache = require("nocache");
 const Razorpay = require('razorpay')
 require('dotenv').config();
+
+console.log("JWT_SECRET:", process.env.JWT_SECRET); // debug for JWT_SECRET
+console.log("JWT_REFRESH_SECRET:", process.env.JWT_REFRESH_SECRET); // debug for JWT_REFRESH_SECRET
+
 require('./config/passport-config'); // Import Passport configuration
 
 const PORT = process.env.PORT;
